c3aa4cac2e9de33cdc57bb0288f13953227fa9c1
devkiloton <dev.kiloton@gmail.com>
2021-10-16 14:21:49 -0300

    validating datas

--- models/services.js
+++ models/services.js
@@ -9,18 +9,45 @@ class Services
         const date = moment(attendance.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
         const allDates = {...attendance, attendanceDate, date};
         
-        const sql = 'INSERT INTO Attendances SET ?';
+        const dateIsValid = moment(date).isSameOrAfter(attendanceDate);
+        const clientIsValid = attendance.client.length >=3;
 
-        connection.query(sql, allDates, (error, results)=>{
-            if(error)
+        const validations = [
             {
-                res.status(400).json(error);
-            }
-            else
+                name: 'date',
+                status: dateIsValid,
+                message: "Date must be greater than or equal to today's date."
+            },
             {
-                res.status(201).json(results);
+                name: 'client',
+                status: clientIsValid,
+                message: "Client name must have at least 3 characters."
             }
-        })
+        ];
+
+        //erros filters the elements in the validations array by a bool in the field status, if at least some of them is true, it is printed
+        const errors = validations.filter(field => !field.status);
+        const isThereErrors = errors.length;
+
+        if(isThereErrors)
+        {
+            res.status(400).json(errors);
+        }
+        else
+        {
+            const sql = 'INSERT INTO Attendances SET ?';
+
+            connection.query(sql, allDates, (error, results)=>{
+                if(error)
+                {
+                    res.status(400).json(error);
+                }
+                else
+                {
+                    res.status(201).json(results);
+                }
+            })
+        }
     }
 }
 
