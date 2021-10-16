0119715dcb0528e14e207f9ab560399b057c2f92
devkiloton <dev.kiloton@gmail.com>
2021-10-15 22:15:55 -0300

    Adding date in the req

--- models/services.js
+++ models/services.js
@@ -1,13 +1,18 @@
 const services = require('../controllers/services');
-const connection = require('../infrastructure/connection')
+const connection = require('../infrastructure/connection');
+const moment = require('moment');
 
 class Services
 {
     add(attendance)
     {
+        const attendanceDate = moment().format('YYYY-MM-DD HH:mm:ss');
+        const date = moment(attendance.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        const allDates = {...attendance, attendanceDate, date};
+        
         const sql = 'INSERT INTO Attendances SET ?';
 
-        connection.query(sql, attendance, (error, results)=>{
+        connection.query(sql, allDates, (error, results)=>{
             if(error)
             {
                 console.log(error);
